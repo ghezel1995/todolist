b9ce299d7d1b8399960f49e9277c2a745e7af6d2
Patron <mahsa.ghezel.1995@gmail.com>
2023-06-11 12:36:58 +0330

    Added (completed or not) button delete button.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
@@ -19,13 +20,94 @@ const TodoList = () => {
     }
   };
 
+  const handleUpdateTodo = async (id, completed) => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${id}`,
+        {
+          method: 'PUT',
+          body: JSON.stringify({ completed }),
+          headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+        }
+      );
+      if (response.ok) {
+        setTodos(
+          todos.map((item) => {
+            if (item.id === id) {
+              return {
+                ...item,
+                completed: !item.completed,
+              };
+            }
+            return item;
+          })
+        );
+      } else {
+        console.error('Error updating todo:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error updating todo:', error);
+    }
+  };
+
+  const handleDeleteTodo = async (id) => {
+    try {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${id}`,
+        {
+          method: 'DELETE',
+        }
+      );
+      if (response.ok) {
+        const updatedTodos = todos.filter((todo) => todo.id !== id);
+        setTodos(updatedTodos);
+      } else {
+        console.error('Error deleting todo:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error deleting todo:', error);
+    }
+  };
+
   return (
     <div className='todo-container'>
       <h1>Todo List</h1>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id} className='todo-li'>
-            {todo.title} - {todo.completed ? 'Completed' : 'Incomplete'}
+            {/* <span>{todo.title}</span> */}
+            <span
+              className='title-container'
+              style={{ textDecoration: todo.underline ? 'underline' : 'none' }}
+            >
+              <i
+                className={`bi ${
+                  todo.completed ? 'bi-hourglass-split' : 'bi-check-square'
+                }`}
+              />
+              {todo.title}
+            </span>
+            <div className='btn-container'>
+              <i
+                className={`bi ${
+                  todo.completed ? 'bi-x-lg' : 'bi bi-check-square'
+                }`}
+                onClick={() => handleUpdateTodo(todo.id, !todo.completed)}
+              >
+                {/* <i
+                className='bi bi-check-square mark-btn'
+                onClick={() => handleUpdateTodo(todo.id, !todo.completed)}
+              > */}
+                {todo.completed ? 'incomplete' : 'Completed'}
+              </i>
+              <i className='bi bi-pencil-square edit-btn'></i>
+              <i
+                className='bi bi-trash3 delete-btn'
+                onClick={() => handleDeleteTodo(todo.id)}
+              ></i>
+            </div>
           </li>
         ))}
       </ul>
